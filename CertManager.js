87bd9dff9863b9a3d04dad7f7ce8074e29d024e6
skcrackers <skcrackers@gmail.com>
2019-07-11 13:03:21 +0900

    File to Base64 추가

--- CertManager.js
+++ CertManager.js
@@ -5,9 +5,7 @@ function base64_encode(file) {
     // read binary data
     var f = fs.readFileSync(file);
     // convert binary data to base64 encoded string
-    return new Buffer(for (var i = 0; i < array.length; i++) {
-      array[i]
-    }).toString('base64');
+    return new Buffer(f).toString('base64');
 }
 
 // function to create file from base64 encoded string
@@ -16,11 +14,11 @@ function base64_decode(base64str, file) {
     var f = new Buffer(base64str, 'base64');
     // write buffer to file
     fs.writeFileSync(file, f);
-    console.log('******** File created from base64 encoded string ********');
 }
 
 // convert image to base64 encoded string
-var base64str = base64_encode('/Users/skcrackers/Documents/cobweb/Modules/certification/ssk/signCert.der');
-console.log(base64str);
-// convert base64 string back to image
-base64_decode(base64str, 'copy.jpg');
+var derFileB64 = base64_encode('/Users/skcrackers/Documents/cobweb/Modules/modules/certification/ssk/signCert.der')
+var keyFileB64 = base64_encode('/Users/skcrackers/Documents/cobweb/Modules/modules/certification/ssk/signPri.key')
+
+console.log('derFileB64 = ' + derFileB64)
+console.log('keyFileB64 = ' + keyFileB64)
